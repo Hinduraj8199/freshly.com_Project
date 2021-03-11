bf85bd636770dee61a6bdea941d37bcdd484edee
hinduraj <hindurajkkadam123@gmail.com>
2021-03-11 13:50:15 +0530

    Final Landing Page

---
+++ Landing Page/Main Landing Page/script.js
@@ -0,0 +1,17 @@
+var boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkPositionOfBoxes);
+checkPositionOfBoxes();
+
+function checkPositionOfBoxes() {
+  var triggerBottom = (window.innerHeight / 5) * 3.75;
+  console.log(triggerBottom);
+  boxes.forEach((box) => {
+    var boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
