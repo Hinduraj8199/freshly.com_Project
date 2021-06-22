fbf05f251a90aa3bb89859d402f5752f0ad7933d
Hinduraj8199 <hindurajkkadam123@gmail.com>
2021-06-23 00:03:19 +0530

    Update Folder structure

--- FAQ Boxes/script.js
+++
@@ -1,7 +0,0 @@
-var toggles = document.querySelectorAll(".faq-toggle");
-
-toggles.forEach((toggle) => {
-  toggle.addEventListener("click", () => {
-    toggle.parentNode.classList.toggle("active");
-  });
-});
